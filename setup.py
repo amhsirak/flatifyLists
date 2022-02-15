d08439ce90ef116de2d362f0804bd6cb6c1b578e
karishmas6 <carishmashukla28@gmail.com>
2022-02-16 05:18:30 +0530

    deployment

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Flatten nested Python lists into a single-depth list'
+
+setup(
+    name="flatify-list",
+    version=VERSION,
+    author="Karishma Shukla",
+    author_email="karishmashuklaa@gmail.com",
+    url="https://github.com/karishmashuklaa/flatify-list",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description= readme(),
+    packages=find_packages(),
+    install_requires=['pytest'],
+    keywords=['flatten', 'lists', 'nested-lists', 'flatten-lists', 'nesting', 'flatify-list'],
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
